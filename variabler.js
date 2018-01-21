b6dc82cdeca3ce16073c132246b5650c8564edc3
Manahil Noor <manahilnoor@Manahils-MBP.lan>
2018-01-21 20:48:40 +0100

    Bootcamp

---
+++ variabler.js
@@ -0,0 +1,25 @@
+
+let navn ="Zoya";
+console.log(navn);
+
+let alder = 23;
+console.log(alder);
+
+let bynavn = "Roskilde";
+console.log(bynavn);
+
+let x = 5;
+let y = 10;
+console.log(x + y);
+
+let z = 15;
+console.log(z);
+
+console.log('Hej, jeg hedder ' + mitFornavn + ' ' + mitEfternavn+ ' og jeg er ' + minAlder + ' år');
+
+let moms = 1.25;
+let prisUdenMoms = 400;
+let prisMedMoms = 400 * 1.25;
+console.log(prisMedMoms);
+let prisUdenMoms2 = prisMedMoms / moms;
+console.log(prisUdenMoms2); 
