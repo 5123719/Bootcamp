61b48d131d386fa525a6fe9c75f4210b8499bb69
Manahil Noor <manahilnoor@Manahils-MacBook-Pro.local>
2018-01-25 16:15:11 +0100

    færdigt med arrays

--- arrays.js
+++ arrays.js
@@ -1,1 +1,32 @@
 
+// øvelse 2 //
+
+let  frugter = ["Pære", "Æble", "Banan"];
+console.log(frugter);
+
+// øvelse 3 //
+
+//let  frugter = ["Pære", "Æble", "Banan"];
+console.log(frugter[1]);
+
+// øvelse 4 //
+
+//let  frugter =["Pære", "Æble", "Banan"];
+frugter[0]= "Fersken";
+console.log(frugter);
+
+// øvelse 5 //
+
+//let  frugter = ["Pære", "Æble", "Banan"];
+console.log(frugter.length);
+
+// øvelse 6 //
+
+frugter[frugter.length] = "Apelsin";
+console.log(frugter);
+
+// øvelse 7 //
+
+let personer = [["Anna", 25, "Roskilde"],["Laila", 24, "Roskilde"],["Zoya", 23, "Roskilde"]];
+console.log(personer);
+
