3fab3a427b8e47a893b6dca5455812347b971708
Manahil Noor <manahilnoor@Manahils-MBP.lan>
2018-02-20 18:25:34 +0100

    færdigt med operatorer.js

---
+++ operatorer.js
@@ -0,0 +1,57 @@
+
+// øvelse 2 //
+
+let forste = 5;
+let anden = 4;
+console.log(forste + anden);
+console.log(forste - anden);
+console.log(forste * anden);
+console.log(forste / anden);
+console.log(forste % anden);
+forste ++;
+anden --;
+
+// øvelse 3 //
+
+// let x = 2;
+// let y = 5;
+// x += y;
+// console.log(x);
+
+// let x = 3;
+// let y = 4;
+// x -= y;
+// console.log(x);
+
+// let x = 6;
+// let y = 8;
+// x *= y;
+// console.log(x);
+
+// let x = 6;
+// let y = 8;
+// x /= y;
+// console.log(x);
+
+ let x = 10;
+ let y = 16;
+ x %= y;
+ console.log(x);
+
+// øvelse 4 //
+
+let number = Math.floor(Math.random() * 10);     // returns a number between 0 and 20
+console.log(number);
+
+if(number > 10){
+    console.log("større end 10");
+}else{
+    console.log("mindre end 10");
+}
+
+// øvelse 5 //
+
+
+if(number > 10){
+    console.log("større end 10");
+}
