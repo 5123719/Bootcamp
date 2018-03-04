4e0a2f154db0895a943feda41b5e456c6c70ce21
Manahil Noor <manahilnoor@Manahils-MBP.lan>
2018-03-04 21:02:31 +0100

    5123719

---
+++ løkker.js
@@ -0,0 +1,39 @@
+
+// opgave 2 //
+
+for (let x = 1; x <= 25; x++){
+    console.log(x);
+}
+
+let count = 1; 
+while(count <= 25){
+  console.log(count);count++;
+}
+
+let counter = 1; 
+
+do{
+    console.log(counter);counter++;}
+   while(counter <= 25)
+
+// opgave 3 //
+
+let y = 1;
+
+do{
+ console.log(y);
+}while(y<=25)
+
+// opgave 4 //
+
+for (let z = 2017; 
+    z>=1917; z--){
+    console.log(z);
+}
+
+// opgave 5 //
+
+let ugedage = Array("mandag", "tirsdag", "onsdag", "torsdag", "fredag", "lørdag", "søndag");
+for(let x = 0; x < ugedage.length; x++){
+    console.log(ugedage[x]);
+}
