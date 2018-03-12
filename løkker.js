b3052c7e668ef4a00a78fba94e9768c5450128e6
Manahil Noor <manahilnoor@Manahils-MacBook-Pro.local>
2018-03-12 15:33:14 +0100

    færdigt med løkker.js

--- løkker.js
+++ løkker.js
@@ -1,20 +1,20 @@
 
 // opgave 2 //
 
-for (let x = 1; x <= 25; x++){
-    console.log(x);
-}
+// for (let x = 1; x <= 25; x++){
+//    console.log(x);
+// }
 
-let count = 1; 
-while(count <= 25){
-  console.log(count);count++;
-}
+// let count = 1; 
+// while(count <= 25){
+//   console.log(count);count++;
+// }
 
-let counter = 1; 
+// let counter = 1; 
 
-do{
-    console.log(counter);counter++;}
-   while(counter <= 25)
+// do{
+//     console.log(counter);counter++;}
+//    while(counter <= 25)
 
 // opgave 3 //
 
@@ -22,6 +22,7 @@ let y = 1;
 
 do{
     console.log(y);
+    y++;
    }while(y<=25)
 
 // opgave 4 //
@@ -37,3 +38,36 @@ let ugedage = Array("mandag", "tirsdag", "onsdag", "torsdag", "fredag", "lørdag
 for(let x = 0; x < ugedage.length; x++){
     console.log(ugedage[x]);
 }
+
+// opgave 7 //
+
+let navne = ["alber", "bob", "carl", "egon", "andreas", "zoya", "sørine", "david"];
+
+navne.forEach(function (navn){
+   console.log(navn);
+});
+
+// opgave 8 //
+
+//let navne = ["Armaan Malik", "Arjit Sing", "Jojo Sing", "Neha Kakkar"];
+
+navne.forEach(function (navn, index) {
+    console.log(navn + "er placeret på " + index + " plads");
+});
+
+// opagve 9 //
+
+let numbers = [1, 23, 40, 150, 50, 8, 11, 80, 30, 15]
+numbers.forEach(function(number, index){
+  if (numbers[index + 1] !=undefined){
+
+    if(number > numbers[index + 1]){
+      console.log(number + "er større end" + numbers[index + 1]);
+    }else{
+      console.log(number + "er mindre end" + numbers[index + 1]);
+    }
+  }
+  else{
+    console.log(number + "har ingen")
+  }
+})
